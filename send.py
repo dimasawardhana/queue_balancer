2f7efd5fbc92dd7ae201a3dba9cd9a0c0ae02f50
eiz3197 <dimasawardhana@gmail.com>
2019-10-01 12:09:34 +0700

    add argument to worker and producer

--- send.py
+++ send.py
@@ -7,10 +7,19 @@ def produce(queue_name, content):
     print content["id"]," sent succesfully"
     
 if __name__ == "__main__":
-    csvPath = "./dataset/articles1.csv"
+    
+    csvSet = {
+        "100" : "./dataset/data_100.csv",
+        "8000" : "./dataset/data_8000.csv",
+        "4000" : "./dataset/data_4000.csv",
+        "articles1": "./dataset/articles1.csv",
+        "articles2": "./dataset/articles2.csv",
+        "articles3": "./dataset/articles3.csv"
+    }
+    csvPath = csvSet[sys.argv[1]]
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
     channel = connection.channel()
-    queue_name = "queue_coba"
+    queue_name = sys.argv[2]
     with open(csvPath) as csvFile:
         csv.field_size_limit(10000000)
         csvReader = csv.DictReader(csvFile)
